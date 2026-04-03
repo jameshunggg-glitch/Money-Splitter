9080f270196c8c01bae462a0bc45db2a3ca9183f
James Hung <jameshunggg@gmail.com>
2026-04-03 21:57:18 +0800

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Users, Utensils, Coffee, Disc, PieChart, Menu, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
